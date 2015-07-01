db5a5576fff33d6fef923bacb9050fc8c716b78b
Brian Kennedy <briankennedy1@gmail.com>
2015-07-01 12:49:31 -0700

    Center some of it

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,85 @@
 $(document).ready(function() {
-  // workingTime();
 
+  questionListener();
+  snapPhoto();
+  savePhoto();
+});
+
+var workingTime = function() {
+  var newDate = $('.start-time').text().prettyDate();
+  console.log(newDate);
+  $('.start-time').text(newDate);
+};
+
+var snapPhoto = function(){
+  var canvas = document.getElementById("canvas"),
+      context = canvas.getContext("2d"),
+      video = document.getElementById("video"),
+      videoObj = { "video": true },
+      errBack = function(error) {
+        console.log("Video capture error: ", error.code);
+      };
+
+  // Put video listeners into place
+  if(navigator.getUserMedia) { // Standard
+    navigator.getUserMedia(videoObj, function(stream) {
+      video.src = stream;
+      video.play();
+    }, errBack);
+  } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+    navigator.webkitGetUserMedia(videoObj, function(stream){
+      video.src = window.URL.createObjectURL(stream);
+      video.play();
+    }, errBack);
+  }
+  else if(navigator.mozGetUserMedia) { // Firefox-prefixed
+    navigator.mozGetUserMedia(videoObj, function(stream){
+      video.src = window.URL.createObjectURL(stream);
+      video.play();
+    }, errBack);
+  };
+  $('#photo-box').on('click','#snap', function(event){
+    context.drawImage(video, 0, 0, 640, 480);
+    $('#video').toggle();
+    $('#snap').toggle();
+  });
+  // document.getElementById("snap").addEventListener("click",
+  //   function() {
+  //     context.drawImage(video, 0, 0, 640, 480);
+  //     $('#video').toggle();
+  //     $('#snap').text('Retake')
+  //     $('#snap').addClass('retake')
+  //   });
+
+  $('#photo-box').on('click','.retake', function(event){
+    console.log('Clicked on retake button')
+  });
+
+};
+
+var savePhoto = function() {
+  var button = document.getElementById('btn-save');
+  button.addEventListener('click', function (e) {
+      var dataURL = canvas.toDataURL('image/png');
+      button.href = dataURL;
+      // Photo.new(path: dataURL)
+      var data = {path: dataURL}
+
+      request = $.ajax({
+        url: '/photos',
+        type: 'POST',
+        data: data
+      });
+      request.done(function(response){
+
+      });
+      request.fail(function(response){
+
+      });
+  });
+};
+
+var questionListener = function() {
   $('.question').first().toggle()
   $('.question').first().addClass('active-question')
   value = $('.question').first().attr('question-id')
@@ -23,68 +102,4 @@ $(document).ready(function() {
     value = next.attr('question-id')
     $('.question_id').attr('value',value)
   })
-
-
-  var canvas = document.getElementById("canvas"),
-  		context = canvas.getContext("2d"),
-  		video = document.getElementById("video"),
-  		videoObj = { "video": true },
-  		errBack = function(error) {
-  			console.log("Video capture error: ", error.code);
-  		};
-
-  	// Put video listeners into place
-  	if(navigator.getUserMedia) { // Standard
-  		navigator.getUserMedia(videoObj, function(stream) {
-  			video.src = stream;
-  			video.play();
-  		}, errBack);
-  	} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-  		navigator.webkitGetUserMedia(videoObj, function(stream){
-  			video.src = window.URL.createObjectURL(stream);
-  			video.play();
-  		}, errBack);
-  	}
-  	else if(navigator.mozGetUserMedia) { // Firefox-prefixed
-  		navigator.mozGetUserMedia(videoObj, function(stream){
-  			video.src = window.URL.createObjectURL(stream);
-  			video.play();
-  		}, errBack);
-  	}
-
-    // Trigger photo take
-    document.getElementById("snap").addEventListener("click", function() {
-    	context.drawImage(video, 0, 0, 640, 480);
-    });
-
-    var button = document.getElementById('btn-save');
-    button.addEventListener('click', function (e) {
-        var dataURL = canvas.toDataURL('image/png');
-        button.href = dataURL;
-        // Photo.new(path: dataURL)
-        var data = {path: dataURL}
-        
-        request = $.ajax({
-          url: '/photos',
-          type: 'POST',
-          data: data,
-          // dataType: "HTML"
-        });
-        request.done(function(response){
-          console.log("SUCCESS");
-          console.log(response);
-        });
-        request.fail(function(response){
-          console.log("FAIL");
-          console.log(response);
-        });
-    });
-
-
-});
-
-var workingTime = function() {
-    var newDate = $('.start-time').text().prettyDate();
-    console.log(newDate);
-    $('.start-time').text(newDate);
 };
