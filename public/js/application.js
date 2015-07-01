f03aaf6aa288c1015ca8d034cca4d6336da7fcc7
Brian Kennedy <briankennedy1@gmail.com>
2015-06-30 19:07:55 -0700

    Show next questions

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,28 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+// We have three questions
+// Grab and display (toggle) first .question inside the .question-box
+
+$('.question').first().toggle()
+$('.question').first().addClass('active-question')
+
+
+$('.question-box').on('click','.next-photo', function(event){
+  event.preventDefault()
+  console.log("You clicked the next button")
+  active = $('.active-question')
+  if(active.html() == $('.question').last().html()){
+    next = $('.question').first()
+  }
+  else {
+    next = $('.active-question').next()
+  }
+  console.log(next)
+  active.toggle()
+  next.toggle()
+  active.removeClass('active-question')
+  next.addClass('active-question')
+})
+
+
 });
